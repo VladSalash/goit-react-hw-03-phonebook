9dc8a86f51e94bcd64a6bfc5fbdec549858b96d8
VladSalash <salashchenko99@gmail.com>
2022-11-02 14:50:41 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,27 @@ addContact = ({name, number}) => {
     return contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter),
 );
-   }
+  }
+
+  componentDidMount() {
+
+       const contacts = localStorage.getItem('contacts');
+        const parsedContacts = JSON.parse(contacts)
+
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts});
+    }
+
+    // console.log(parsedContacts)
+
+
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
 
   render() {
     const { filter} = this.state;
