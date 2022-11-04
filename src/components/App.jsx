4f412c9494f50300bc88515df00d654ef1218ee9
VladSalash <salashchenko99@gmail.com>
2022-11-04 15:20:33 +0300

    corrections to the code after checking the mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -38,9 +33,7 @@ export class App extends Component {
     const stateContacts = [...contacts];
 
     const existContact = contacts.filter(contact => {
-      return contact.name.toLocaleLowerCase.includes(
-        data.name.toLocaleLowerCase()
-      );
+      return contact.name.toLowerCase().includes(data.name.toLowerCase());
     });
 
     if (existContact.length > 0) {
